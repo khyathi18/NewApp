9d68f1ff3b4465f15196f24751e8a00699774c54
khyathi18 <ksri0307@gmail.com>
2022-05-25 13:48:12 +0530

    appcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { SecComponent } from './sec/sec.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    SecComponent
   ],
   imports: [
     BrowserModule,
